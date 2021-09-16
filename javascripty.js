016114b10f0fde4b7c2abb544ce0b1433ad09bbe
Joao Paulo <joaopaulo92803693@gmail.com>
2021-09-15 22:25:17 -0300

    inicial

---
+++ javascripty.js
@@ -0,0 +1,18 @@
+var nomeVendedor = prompt('digite o nome do vendedor');
+var salarioFixo = prompt('digite o salario do vendedor');
+var totalVendas = prompt('digite o valor das vendas do vendedor');
+
+
+
+function salarioTotal (vendedor)  {
+    return salarioFixo + 0,15 * totalVendas
+}
+
+console.log(salarioTotal)
+
+
+
+
+
+
+

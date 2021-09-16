4210f5653603c699a6647eac4e3013a8fa43a346
Joao Paulo <joaopaulo92803693@gmail.com>
2021-09-15 22:40:58 -0300

    reduzindo codigo

--- javascripty.js
+++ javascripty.js
@@ -1,18 +1,11 @@
-function salarioTotal() {
-    var vendedor = {
-      nome: "joão",
-      salarioFixo: 1400,
-      totalVendas: 7000,
-      comissao: 15
-    };
+function salarioTotal(nome, salarioFixo, total, porcentagem) {
+    let comissao = parseFloat(total * (porcentagem / 100));
+    let salario = parseFloat(salarioFixo + comissao);
   
-    let comissao = parseFloat(vendedor.totalVendas * (15 / 100));
-    let salario = parseFloat(vendedor.salarioFixo + comissao);
+    return `O salário do vendedor ${nome} foi de R$ ${salario.toFixed(2)} no final do mês, sendo ${salarioFixo} de fixo + ${comissao} de comissao`;
+}
   
-    return `O salário do vendedor ${vendedor.nome} foi de R$ ${salario.toFixed(2)} no final do mês, sendo ${vendedor.salarioFixo} de fixo + ${comissao} de comissao`;
-  }
-  
-  console.log(salarioTotal());
+console.log(salarioTotal('João', 1400, 7000, 15));
   
   
   

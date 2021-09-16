a22e4a31c01dee565243181e9d10238bde5b1e25
Joao Paulo <joaopaulo92803693@gmail.com>
2021-09-15 22:36:02 -0300

    ajustando return da função

--- javascripty.js
+++ javascripty.js
@@ -1,15 +1,24 @@
-var nomeVendedor = prompt('digite o nome do vendedor');
-var salarioFixo = prompt('digite o salario do vendedor');
-var totalVendas = prompt('digite o valor das vendas do vendedor');
-
-
-
-function salarioTotal (vendedor)  {
-    return salarioFixo + 0,15 * totalVendas
-}
-
-console.log(salarioTotal)
-
+function salarioTotal() {
+    var vendedor = {
+      nome: "joão",
+      salarioFixo: 1400,
+      totalVendas: 7000,
+      comissao: 15
+    };
+  
+    let comissao = parseFloat(vendedor.totalVendas * (15 / 100));
+    let salario = parseFloat(vendedor.salarioFixo + comissao);
+  
+    return `O salário do vendedor ${vendedor.nome} foi de R$ ${salario.toFixed(2)} no final do mês, sendo ${vendedor.salarioFixo} de fixo + ${comissao} de comissao`;
+  }
+  
+  console.log(salarioTotal());
+  
+  
+  
+  
+  
+  
 
 
 
